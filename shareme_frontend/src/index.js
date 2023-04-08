5bb736f311414ea8af20b7fe63ee85a526d9a3f8
fmintar1 <fmintar1@gmail.com>
2023-04-08 02:07:21 -0400

    added component and container folder with login and home files

--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 
 //hook App to the real DOM, which is document.getElementById
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root')
+  );
